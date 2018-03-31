a3babc041dc43e3e663832b74d1c7b72b03aa6ce
Erin Banister <erin.banister@gmail.com>
2018-03-31 16:00:48 -0600

    Update to include dimen.xml and created global instance variables to minimize the findViewById usage

--- app/src/main/java/com/example/android/sciencequiz/MainActivity.java
+++ app/src/main/java/com/example/android/sciencequiz/MainActivity.java
@@ -29,14 +29,67 @@ public class MainActivity extends AppCompatActivity {
     //set currentPoints value to 0 for new scoring session
     public int currentPoints = 0;
 
+    public Button resetButton;
+    public EditText quizAnswer1;
+    public TextView answer1;
+    public CheckBox quizAnswer2a;
+    public CheckBox quizAnswer2b;
+    public CheckBox quizAnswer2c;
+    public CheckBox quizAnswer2d;
+    public CheckBox quizAnswer2e;
+    public TextView answer2;
+    public EditText quizAnswer3;
+    public TextView answer3;
+    public RadioGroup answer4group;
+    public RadioGroup answer6group;
+    public RadioButton quizAnswer4a;
+    public RadioButton quizAnswer4b;
+    public RadioButton quizAnswer4c;
+    public RadioButton quizAnswer4d;
+    public TextView answer4;
+    public EditText quizAnswer5;
+    public TextView answer5;
+    public RadioButton quizAnswer6a;
+    public RadioButton quizAnswer6b;
+    public TextView answer6;
+    public TextView Total;
+    public ImageView lightBulb;
+    public Button reset;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        resetButton= findViewById(R.id.reset);
+        quizAnswer1 = findViewById(R.id.quiz_answer_1);
+        answer1 =  findViewById(R.id.answer1);
+        quizAnswer2a= findViewById(R.id.quiz_answer_2a);
+        quizAnswer2b= findViewById(R.id.quiz_answer_2b);
+        quizAnswer2c= findViewById(R.id.quiz_answer_2c);
+        quizAnswer2d= findViewById(R.id.quiz_answer_2d);
+        quizAnswer2e= findViewById(R.id.quiz_answer_2e);
+        answer2 = findViewById(R.id.answer2);
+        quizAnswer3= findViewById(R.id.quiz_answer_3);
+        answer3= findViewById(R.id.answer3);
+        answer4group = findViewById(R.id.question_4_group);
+        answer6group = findViewById(R.id.question_6_group);
+        quizAnswer4a= findViewById(R.id.quiz_answer_4a);
+        quizAnswer4b= findViewById(R.id.quiz_answer_4b);
+        quizAnswer4c= findViewById(R.id.quiz_answer_4c);
+        quizAnswer4d= findViewById(R.id.quiz_answer_4d);
+        answer4= findViewById(R.id.answer4);
+        quizAnswer5= findViewById(R.id.quiz_answer_5);
+        answer5= findViewById(R.id.answer5);
+        quizAnswer6a= findViewById(R.id.quiz_answer_6a);
+        quizAnswer6b= findViewById(R.id.quiz_answer_6b);
+        answer6= findViewById(R.id.answer6);
+        Total = findViewById(R.id.Total);
+        lightBulb = findViewById(R.id.lightbulb);
+
         //create an onClickListener for the reset button, and runs function resetAnswers onCilck
-        Button reset = findViewById(R.id.reset);
-        reset.setOnClickListener(new View.OnClickListener() {
+        resetButton.setOnClickListener(//set all id question/answer id values to public variables, to speed process times
+        new View.OnClickListener() {
             public void onClick(View view) {
                 resetAnswers();
             }
@@ -59,58 +112,51 @@ public class MainActivity extends AppCompatActivity {
         String answer;
 
         //get string response to answer1, then compare answer1 to correct answer, log points
-
-        String userEntry = ((EditText) findViewById(R.id.quiz_answer_1)).getText().toString();
-        checkEditText(userEntry, getString(R.string.question1_answer), getString(R.string.question1_answer_2), R.id.answer1);
+        String userEntry = quizAnswer1.getText().toString();
+        checkEditText(userEntry, getString(R.string.question1_answer), getString(R.string.question1_answer_2), answer1);
 
         // compare answer2 to correct answer
         int question2 = 0;  //there are 3 right answers in this question. This keeps total point value to ensure question is marked wrong if <3 points are received.
         int question2_wrong = 0;
 
-        CheckBox checkbox = findViewById(R.id.quiz_answer_2a);
-
         //a,b,c is true
-        if (checkbox.isChecked()) {
+        if (quizAnswer2a.isChecked()) {
             question2 = question2 + 1;
-            setCorrect(R.id.quiz_answer_2a, true);
+            setCorrect(quizAnswer2a, true);
         }
 
-        checkbox = findViewById(R.id.quiz_answer_2b);
-        if (checkbox.isChecked()) {
+        if (quizAnswer2b.isChecked()) {
             question2 = question2 + 1;
-            setCorrect(R.id.quiz_answer_2b, true);
+            setCorrect(quizAnswer2b, true);
         }
 
-        checkbox = findViewById(R.id.quiz_answer_2c);
-        if (checkbox.isChecked()) {
+        if (quizAnswer2c.isChecked()) {
             question2 = question2 + 1;
-            setCorrect(R.id.quiz_answer_2c, true);
+            setCorrect(quizAnswer2c, true);
         }
 
-        checkbox = findViewById(R.id.quiz_answer_2d);
-        if (checkbox.isChecked()) {
-            setCorrect(R.id.quiz_answer_2d, false);
+        if (quizAnswer2d.isChecked()) {
+            setCorrect(quizAnswer2d, false);
             question2_wrong = question2_wrong+1;
         }
 
-        checkbox = findViewById(R.id.quiz_answer_2e);
-        if (checkbox.isChecked()) {
-            setCorrect(R.id.quiz_answer_2e, false);
+        if (quizAnswer2e.isChecked()) {
+            setCorrect(quizAnswer2e, false);
             question2_wrong = question2_wrong+1;
         }
 
         if (question2 < 3) {
             answer = getString(R.string.question2_answer);
-            updateAnswer(R.id.answer2, answer, false);
+            updateAnswer(answer2, answer, false);
         } else if (question2 == 3) {
             //check if there are wrong answers included as well
             if (question2_wrong>0) {
                 answer = getString(R.string.question2_answer_someWrong);
-                ((TextView) findViewById(R.id.answer2)).setText(answer);
-                setCorrect(R.id.answer2, true);
+                answer2.setText(answer);
+                setCorrect(answer2, true);
             } else {
                 answer = getString(R.string.correct_answer);
-                updateAnswer(R.id.answer2, answer, true);
+                updateAnswer(answer2, answer, true);
             }
         } else {
             Toast toast = Toast.makeText(getApplicationContext(), R.string.question2_wrongPoints, Toast.LENGTH_SHORT);
@@ -122,23 +168,21 @@ public class MainActivity extends AppCompatActivity {
 
         // compare answer3 to correct answer,then log points
 
-        userEntry = ((EditText) findViewById(R.id.quiz_answer_3)).getText().toString();
-        checkEditText(userEntry, getString(R.string.question3_answer), getString(R.string.question3_answer), R.id.answer3);
+        userEntry = quizAnswer3.getText().toString();
+        checkEditText(userEntry, getString(R.string.question3_answer), getString(R.string.question3_answer), answer3);
 
         //compare answer 4 to correct answer
-        RadioGroup question4 = findViewById(R.id.question_4_group);
-        int selectedValueId = question4.getCheckedRadioButtonId();
-        checkRadio(selectedValueId, R.id.quiz_answer_4d, R.id.answer4, getString(R.string.question4_answer));
+        RadioButton selectedValueId = findViewById(answer4group.getCheckedRadioButtonId());
+        checkRadio(selectedValueId,quizAnswer4d, answer4, getString(R.string.question4_answer));
 
         //compare answer 5 to correct answer, then compare answer1 to correct answer, log points
 
         userEntry = ((EditText) findViewById(R.id.quiz_answer_5)).getText().toString();
-        checkEditText(userEntry, getString(R.string.question5_answer), getString(R.string.question5_answer_2), R.id.answer5);
+        checkEditText(userEntry, getString(R.string.question5_answer), getString(R.string.question5_answer_2), answer5);
 
         //compare answer 6 to correct answer
-        RadioGroup question6 = findViewById(R.id.question_6_group);
-        selectedValueId = question6.getCheckedRadioButtonId();
-        checkRadio(selectedValueId, R.id.quiz_answer_6a, R.id.answer6, getString(R.string.question6_answer));
+        selectedValueId = findViewById(answer6group.getCheckedRadioButtonId());
+        checkRadio(selectedValueId, quizAnswer6a, answer6, getString(R.string.question6_answer));
 
         //Calculate score
         String message = getString(R.string.total, currentPoints);
@@ -146,19 +190,22 @@ public class MainActivity extends AppCompatActivity {
 
         //if score > 60, light bulb lights!
         if (scorePercent >= 60) {
-            ((ImageView) findViewById(R.id.lightbulb)).setImageResource(R.drawable.lighton);
+            lightBulb.setImageResource(R.drawable.lighton);
         }
 
         //make toast
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 
-        //create & display total message in Total TextViwe
+        //create & display total message in Total TextView
         message = message + " This is a score of " + scorePercent + "%!";
-        ((TextView) findViewById(R.id.Total)).setText(message);
+        Total.setText(message);
 
         //scroll to top of quiz to see answers
         ScrollView sv = findViewById(R.id.myScroll);
         sv.scrollTo(0, 0);
+
+        //reset points
+        currentPoints = 0;
     }
 
     /**
@@ -170,7 +217,7 @@ public class MainActivity extends AppCompatActivity {
      * @param possibleAnswer2 - possible answer 2 (last name only)
      * @param answerID        - textView ID where the answer should be inputted after.
      */
-    private void checkEditText(String answer, String possibleAnswer1, String possibleAnswer2, int answerID) {
+    private void checkEditText(String answer, String possibleAnswer1, String possibleAnswer2, TextView answerID) {
 
         if (answer.trim().equalsIgnoreCase(possibleAnswer1.trim())) {
             currentPoints = currentPoints + 1;
@@ -196,7 +243,7 @@ public class MainActivity extends AppCompatActivity {
      * @param answerID        - TextView id where the answer is applied
      * @param answer          - String of correct answer text.
      */
-    private void checkRadio(int selectedValueId, int correctValue, int answerID, String answer) {
+    private void checkRadio(RadioButton selectedValueId, RadioButton correctValue, TextView answerID, String answer) {
 
         if (selectedValueId == correctValue) {
             //correct
@@ -218,10 +265,10 @@ public class MainActivity extends AppCompatActivity {
      * @param viewID is the number value of the view
      * @param answer is the answer for the question referenced
      */
-    public void updateAnswer(int viewID, String answer, boolean correct) {
+    public void updateAnswer(TextView viewID, String answer, boolean correct) {
 
         String finalAnswer = correct ? getString(R.string.correct_answer) : getString(R.string.incorrect_answer) + " " + answer;
-        ((TextView) findViewById(viewID)).setText(finalAnswer);
+        viewID.setText(finalAnswer);
         setCorrect(viewID, correct);
     }
 
@@ -229,15 +276,15 @@ public class MainActivity extends AppCompatActivity {
      * The setCorrect function updates the textColor values of answers after submitting.
      * Correct is green; incorrect is red
      *
-     * @param viewId  the int view id of the text object being changed
+     * TextView viewId is the view id of the text object being changed
      * @param correct a boolean value corresponding with right/wrong answers
      */
-    public void setCorrect(int viewId, boolean correct) {
+    public void setCorrect(TextView viewID, boolean correct) {
         //the ? operator says that if the colorId is TRUE, that it uses the correct color (green),
         //otherwise it will use the incorrect color (red) - both of which are in colors.xml
         int colorId = correct ? R.color.correct : R.color.incorrect;
         int color = ContextCompat.getColor(getApplicationContext(), colorId);
-        ((TextView) findViewById(viewId)).setTextColor(color);
+        viewID.setTextColor(color);
     }
 
     /**
@@ -248,37 +295,43 @@ public class MainActivity extends AppCompatActivity {
         //get integer of color number for resetting RadioButtons & CheckBoxes
         int standardColor = getResources().getColor(R.color.standardText);
         // Reset EditText values & colors
-        ((EditText) findViewById(R.id.quiz_answer_1)).setText("");
-        (findViewById(R.id.quiz_answer_1)).getResources().getColor(R.color.standardText);
-        ((EditText) findViewById(R.id.quiz_answer_3)).setText("");
-        (findViewById(R.id.quiz_answer_3)).getResources().getColor(R.color.standardText);
-        ((EditText) findViewById(R.id.quiz_answer_5)).setText("");
-        (findViewById(R.id.quiz_answer_5)).getResources().getColor(R.color.standardText);
+        quizAnswer1.setText("");
+        quizAnswer1.setTextColor(standardColor);
+        quizAnswer3.setText("");
+        quizAnswer3.setTextColor(standardColor);
+        quizAnswer5.setText("");
+        quizAnswer5.setTextColor(standardColor);
         // Reset RadioGroup selections and colors
-        RadioGroup rg = findViewById(R.id.question_4_group);
-        rg.clearCheck();
+        answer4group.clearCheck();
         //reset question 4 group text & color
-        ((RadioButton) findViewById(R.id.quiz_answer_4a)).setTextColor(standardColor);
-        ((RadioButton) findViewById(R.id.quiz_answer_4b)).setTextColor(standardColor);
-        ((RadioButton) findViewById(R.id.quiz_answer_4c)).setTextColor(standardColor);
-        ((RadioButton) findViewById(R.id.quiz_answer_4d)).setTextColor(standardColor);
+        quizAnswer4a.setTextColor(standardColor);
+        quizAnswer4b.setTextColor(standardColor);
+        quizAnswer4c.setTextColor(standardColor);
+        quizAnswer4d.setTextColor(standardColor);
         //reset question 6 group text & color
-        rg = findViewById(R.id.question_6_group);
-        rg.clearCheck();
-        ((RadioButton) findViewById(R.id.quiz_answer_6a)).setTextColor(standardColor);
-        ((RadioButton) findViewById(R.id.quiz_answer_6b)).setTextColor(standardColor);
+        answer6group.clearCheck();
+        quizAnswer6a.setTextColor(standardColor);
+        quizAnswer6b.setTextColor(standardColor);
         //reset value and color of all CheckBox views
-        ((CheckBox) findViewById(R.id.quiz_answer_2a)).setChecked(false);
-        ((CheckBox) findViewById(R.id.quiz_answer_2a)).setTextColor(standardColor);
-        ((CheckBox) findViewById(R.id.quiz_answer_2b)).setChecked(false);
-        ((CheckBox) findViewById(R.id.quiz_answer_2b)).setTextColor(standardColor);
-        ((CheckBox) findViewById(R.id.quiz_answer_2c)).setChecked(false);
-        ((CheckBox) findViewById(R.id.quiz_answer_2c)).setTextColor(standardColor);
-        ((CheckBox) findViewById(R.id.quiz_answer_2d)).setChecked(false);
-        ((CheckBox) findViewById(R.id.quiz_answer_2d)).setTextColor(standardColor);
-        ((CheckBox) findViewById(R.id.quiz_answer_2e)).setChecked(false);
-        ((CheckBox) findViewById(R.id.quiz_answer_2e)).setTextColor(standardColor);
-        //set value of all TextViews to empty strings ("")
+        quizAnswer2a.setChecked(false);
+        quizAnswer2a.setTextColor(standardColor);
+        quizAnswer2b.setChecked(false);
+        quizAnswer2b.setTextColor(standardColor);
+        quizAnswer2c.setChecked(false);
+        quizAnswer2c.setTextColor(standardColor);
+        quizAnswer2d.setChecked(false);
+        quizAnswer2d.setTextColor(standardColor);
+        quizAnswer2e.setChecked(false);
+        quizAnswer2e.setTextColor(standardColor);
+         //set value of all TextViews to empty strings ("")
+        answer1.setText("");
+        answer2.setText("");
+        answer3.setText("");
+        answer4.setText("");
+        answer5.setText("");
+        answer6.setText("");
+        Total.setText("");
+
         ((TextView) findViewById(R.id.answer1)).setText("");
         ((TextView) findViewById(R.id.answer2)).setText("");
         ((TextView) findViewById(R.id.answer3)).setText("");
@@ -287,7 +340,7 @@ public class MainActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.answer6)).setText("");
         ((TextView) findViewById(R.id.Total)).setText("");
         //Turn off the light!
-        ((ImageView) findViewById(R.id.lightbulb)).setImageResource(R.drawable.lightoff);
+        lightBulb.setImageResource(R.drawable.lightoff);
         //scroll to top of quiz
         ScrollView sv = findViewById(R.id.myScroll);
         sv.scrollTo(0, 0);
